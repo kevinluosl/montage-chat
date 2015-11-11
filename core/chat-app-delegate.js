33a2ef0b607f6a42fb82e2209a2f5dc8eb237c44
TOM WANG <tom>
2015-11-11 10:53:06 +0800

    Using Aly Openfire server

--- core/chat-app-delegate.js
+++ core/chat-app-delegate.js
@@ -3,8 +3,8 @@ var Montage = require("montage/core/core").Montage;
 exports.ChatAppDelegate = Montage.specialize({
     willFinishLoading: {value: function (app) {
         app.config = {
-            BOSH_SERVICE: "http://192.168.1.238:7070/http-bind/",
-            SERVICE_DOMAIN:"conference.toms-mbp"
+            BOSH_SERVICE: "http://115.28.165.154:7070/http-bind/",
+            SERVICE_DOMAIN:"conference.contour"
         };
     }}
 });
